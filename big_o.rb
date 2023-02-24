5a23fb4f6812b0fe1b5387eb7b04f46114b1c107
Bilal <bilalachoudhry@gmail.com>
2023-02-24 14:47:27 -0800

    second question

--- big_o.rb
+++ big_o.rb
@@ -28,9 +28,47 @@
 #     return min
 # end
 
-list = [ 0, 3, 5, 4, -5, 10, 1, 90 ]
-p my_min(list)  # =>  -5
+# list = [ 0, 3, 5, 4, -5, 10, 1, 90 ]
+# p my_min(list)  # =>  -5
 
 #Time complexity: Linear
 #------------------------------------------------------------------------------------------------------------
+
+#Phase 1
+
+# def largest_contiguous_subsum(array) 
+#     subsets = []
+
+#     array.each_with_index do |ele1, idx1|  
+#         array.each_with_index do |ele2, idx2| 
+#             if idx2 >= idx1
+#                 sum_array = array[idx1..idx2].sum
+#                 subsets << sum_array
+#             end
+#         end
+#     end
+#     subsets.max
+# end 
+
+#Time Complexity = Exponential 
+
+#Phase 2
+
+# def largest_contiguous_subsum(array) #[-7,5,3]
+#     largest_sum = array[0] #-7 => -2 => 1
+#     current_sum = array[0] #-7 => -2 => 1
+
+#     (1...array.length).each do |i| #5 => next ite: 3
+#         if current_sum < 0 
+#             current_sum = 0
+#         end
+
+#         current_sum = current_sum + array[i] #-2 + 3 => 1 
+#         largest_sum = current_sum if current_sum > largest_sum #1 => -2  => 1
+#     end 
+#     largest_sum
+# end 
+
+#Time Complexity = Linear
+
 
